e197e7a19b2d7506485fc9f2947718d466e3d1ad
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2022-07-06 21:27:43 -0300

    Uso de Generics e CompareTo

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,6 @@ public class Program {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-
         List<Integer> list = new ArrayList<>();
 
         String path = "C:\\temp\\in.txt";
